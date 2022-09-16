19f601d853aec3ca6a8e2735a9b889dc4d694848
Helen <helenlena52@gmail.com>
2022-09-16 16:59:38 +0300

    add-second-gallery-markup

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listGalleryRef = document.querySelector('.gallery')
+
+listGalleryRef.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems))
+listGalleryRef.addEventListener('click', simpleGalleryHandler)
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`
+    }).join('');
+}
+
+function simpleGalleryHandler(event) { 
+    event.preventDefault()
+
+    if (event.target !== "IMG") { 
+        return
+    }
+    let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// event.target.dataset.source = 
+});
+}
